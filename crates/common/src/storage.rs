09e6142a9b3a621be3c19099cccaf0c9ab2bab9f
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-02-04 12:45:24 -0600

    create storage struct and begin atomic write procedure

--- crates/common/src/storage.rs
+++ crates/common/src/storage.rs
@@ -1,34 +1,78 @@
 use std::{
     env::{consts::OS, home_dir},
     fs::{File, create_dir_all},
-    path::PathBuf,
+    io::{BufReader, Error, Read},
+    path::{Path, PathBuf},
 };
 
-// The main files are stored in ~/.csync
-// Manifests are stored in ~/.csync/manifests/
-// Data is stored in ~/.csync/data/
-// Configs are stored in ~/.csync/config.toml
-pub fn init_filesystem() -> Result<PathBuf, String> {
-    // Server MUST be Linux based. For now...
-    if OS != "linux" {
-        return Err("csync currently only supports Linux operating systems".to_string());
+use crate::hash::hash_bytes;
+
+pub enum StorageError {
+    UnsupportedOS,
+    NoHomeDir,
+    PathOutsideRoot,
+    InvalidFileLocation,
+}
+
+pub struct Storage {
+    pub root: PathBuf,
+}
+
+impl Storage {
+    pub fn new() -> Result<Self, StorageError> {
+        if OS != "linux" {
+            return Err(StorageError::UnsupportedOS);
+        }
+
+        let home_dir = home_dir().ok_or(StorageError::NoHomeDir)?;
+        let root = home_dir.join(".csync");
+
+        let data = root.join("data");
+        let manifests = root.join("manifests");
+        let config = root.join("config.toml");
+
+        create_dir_all(&data).unwrap();
+        create_dir_all(&manifests).unwrap();
+
+        if !config.exists() {
+            File::create(&config).unwrap();
+        }
+
+        Ok(Self { root })
     }
 
-    let home_dir = home_dir().unwrap();
-    println!("Your home dir {:?}", home_dir);
-
-    // Create ~/.csync and all children if not exists
-    let base_dir = home_dir.join(".csync");
-    let data_dir = base_dir.join("data");
-    let manifest_dir = base_dir.join("manifests");
-    let config_path = base_dir.join("config.toml");
-
-    create_dir_all(data_dir).map_err(|e| e.to_string())?;
-    create_dir_all(manifest_dir).map_err(|e| e.to_string())?;
-
-    if !config_path.exists() {
-        File::create(&config_path).map_err(|e| e.to_string())?;
+    pub fn data_dir(&self) -> PathBuf {
+        self.root.join("data")
     }
 
-    Ok(base_dir.to_path_buf())
+    pub fn manifest_dir(&self) -> PathBuf {
+        self.root.join("manifests")
+    }
+
+    pub fn config_path(&self) -> PathBuf {
+        self.root.join("config.toml")
+    }
+
+    pub fn atomic_write(&self, path: &Path, bytes: &[u8]) -> Result<(), StorageError> {
+        // Firstly, ensure the path is a child of the root to remove
+        // external write invariant
+
+        let is_child_path = path.starts_with(&self.root);
+        if !is_child_path {
+            return Err(StorageError::PathOutsideRoot);
+        }
+
+        let filename = path.file_name().ok_or(StorageError::InvalidFileLocation)?;
+        let path_root = path.parent().ok_or(StorageError::InvalidFileLocation)?;
+        // Then, create a new file called the filename + .temp
+        let mut temp_file = PathBuf::from(path_root);
+        temp_file.push(filename);
+        temp_file.set_extension(".temp");
+
+        // Write to the temp file in the same dir
+        // Then renaming the file to the write file is atomic
+        // Done!
+
+        Ok(())
+    }
 }
