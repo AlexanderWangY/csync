5187fdce73f4d4e01bd0a7d87210b52c2cb3a67d
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-02-09 15:43:13 -0500

    some path and hexa shenanigans

--- crates/common/src/cas.rs
+++ crates/common/src/cas.rs
@@ -1,6 +1,18 @@
-use std::sync::Arc;
+use std::{fs, sync::Arc};
 
-use crate::storage::Storage;
+use crate::storage::{Storage, StorageError};
+
+#[derive(Debug)]
+pub enum CasError {
+    StorageErr(StorageError),
+    Unknown,
+}
+
+impl From<StorageError> for CasError {
+    fn from(value: StorageError) -> Self {
+        Self::StorageErr(value)
+    }
+}
 
 pub struct Cas {
     pub storage: Arc<Storage>,
@@ -22,4 +34,33 @@ impl Cas {
     pub fn new_with_storage(storage: Arc<Storage>) -> Self {
         Self { storage }
     }
+
+    pub fn put_object(&self, hash: &[u8; 32], data: &[u8]) -> Result<(), CasError> {
+        // Crawl through 2 layer data layer
+        // 1) Layer 1 doesnt exist, please create
+        // 2) Layer 2 doent exist, please create
+        // 3) Use storage.atomic_write() to write into a file with name = hash
+
+        let objects_dir = self.storage.objects_dir();
+
+        // Formats first 2 bytes of hash to hex representation
+        let shard1 = format!("{:02x}", hash[0]);
+        let shard2 = format!("{:02x}", hash[1]);
+        let object_name = hex::encode(data);
+
+        // Build object store shard path
+        let shard_dir = objects_dir.join(shard1).join(shard2);
+        let shard_object_path = shard_dir.join(object_name);
+
+        // If already exists, do NOT write
+        if shard_object_path.exists() {
+            return Ok(());
+        }
+
+        self.storage.atomic_write(&shard_object_path, data)?;
+
+        Ok(())
+    }
+
+    pub fn get_object(&self, hash: &[u8; 32]) {}
 }
